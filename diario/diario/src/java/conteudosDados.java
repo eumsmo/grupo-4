27e702ba78cdd4242a50b61cef9f2ae24201e055
Marcus <marcusarcanjo02@gmail.com>
2019-10-22 17:31:39 -0300

    Conteúdos dados

---
+++ diario/diario/src/java/conteudosDados.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Marcus
+ */
+@WebServlet(urlPatterns = {"/NewServlet"})
+public class conteudosDados extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    final String SERVIDOR_SQL = "jdbc:mysql://localhost:3307/diario",
+	    USUARIO_ADMIN_SQL = "root",
+	    SENHA_ADMIN_SQL = "123456";
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        
+            
+            
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           try{
+            String query;
+           query = "SELECT * FROM descartes";
+           DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+		Connection conexao = DriverManager.getConnection(SERVIDOR_SQL, USUARIO_ADMIN_SQL, SENHA_ADMIN_SQL);
+		PreparedStatement st = conexao.prepareStatement(query);
+
+		ResultSet resultado = st.executeQuery();
+                
+                while (resultado.next()) {
+		    int etapa = resultado.getInt("idEtapas");
+                    String disciplina = resultado.getString("idDisciplinas"),
+		    conteudo = resultado.getString("conteudos");
+                    Date data = resultado.getDate("datas");
+
+		    out.println("Etapa: " + etapa + ", disciplina: " + disciplina + ", conteudo: '" + conteudo + "', data: " + data);
+		}
+
+		st.close();
+		conexao.close();
+
+           }catch (SQLException e) {
+		out.println("ERRO DE LEITURA/CONEXÃO NA TABELA 'CONTEUDOS' - " + e.getMessage());
+		return;
+	    }
+           
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
